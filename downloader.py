4a5a28fdaad63d9b626d734f5f59781fd1a5d2f2
Rajat Khanduja <rajatkhanduja13@gmail.com>
2011-04-14 23:35:12 +0530

    Added license

--- downloader.py
+++ downloader.py
@@ -1,6 +1,25 @@
 #!/usr/bin/bash
 # Author : Rajat Khanduja
 # This file describes the downloader class
+#
+#
+# “Copyright 2011 Rajat Khanduja”
+#
+# This file is part of Pycurl-Downloader.
+#
+# Pycurl-Downloader is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pycurl-Downloader is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pycurl-Downloader.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import pycurl
 import os,sys
